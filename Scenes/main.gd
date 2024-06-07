2ff74f4b43092e1570d03a09c5aa382d453efa62
Brian <brianhall1313@gmail.com>
2024-06-07 13:06:48 -0700

    initial commit

---
+++ Scenes/main.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+
+
+func _on_quit_button_up():
+	get_tree().quit()
+
+
+func _on_new_game_button_up():
+	get_tree().change_scene_to_file("res://Scenes/field.tscn")
+
+
+func _on_credits_button_up():
+	$Control/credits.show()
+
+func _on_how_to_play_button_up():
+	$"Control/how to play".show()
+
+
+func _on_how_to_play_button_back_button_up():
+	$"Control/how to play".hide()
+
+
+func _on_credits_back_button_up():
+	$Control/credits.hide()
+
---
+++ Scenes/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const SPEED:float = 700.0
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left"):
+		velocity.x -= 0.1
+	elif Input.is_action_pressed("right"):
+		velocity.x += 0.1
+	else:
+		if velocity.x > 0:
+			velocity.x -= 0.35
+		elif velocity.x < 0:
+			velocity.x += 0.35
+		if abs(velocity.x) < 0.35:
+			velocity = Vector2.ZERO
+	if abs(velocity.x) > 1:
+		velocity.x = velocity.x / abs(velocity.x)
+	var result = move_and_collide(velocity * delta * SPEED)
+	if result:
+		velocity = Vector2.ZERO
